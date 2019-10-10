b3492cfd6c7d8bc374b41e6c2c8ed882c58f06f8
belu82 <44766864+belu82@users.noreply.github.com>
2019-10-10 16:37:52 +0200

    Finally works

--- TimerEvents3/Program.cs
+++ TimerEvents3/Program.cs
@@ -15,6 +15,7 @@ namespace TimerEvents3
         static void Main()
         {
 
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
